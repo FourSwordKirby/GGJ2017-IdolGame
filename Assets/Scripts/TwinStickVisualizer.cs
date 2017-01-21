00f722b7c980701438a46050ec37f8600b10e176
Stephen Chen <stephen1@andrew.cmu.edu>
2017-01-21 14:23:50 -0500

    PS4 controller visualizer.

--- Assets/Scripts/TwinStickVisualizer.cs
+++ Assets/Scripts/TwinStickVisualizer.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class TwinStickVisualizer : MonoBehaviour {
 
     public TwinStickControls controls;
-    private Animator anim;
+    public RectTransform LeftPanel;
+    public RectTransform RightPanel;
+    public RectTransform LStickIcon;
+    public RectTransform RStickIcon;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		if (!controls)
         {
             controls = GameObject.FindObjectOfType<TwinStickControls>();
@@ -17,12 +20,26 @@ public class TwinStickVisualizer : MonoBehaviour {
                 Debug.LogError("Can't find TwinStick controls");
             }
         }
+        LeftPanel = this.transform.FindChild("Left").GetComponent<RectTransform>();
+        RightPanel = this.transform.FindChild("Right").GetComponent<RectTransform>();
+        LStickIcon = LeftPanel.FindChild("LStick").GetComponent<RectTransform>();
+        RStickIcon = RightPanel.FindChild("RStick").GetComponent<RectTransform>();
 
-        anim = this.GetComponent<Animator>();
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        Debug.Log("" + TwinStickControls.getLeftDirection() + " " + TwinStickControls.getRightDirection());
+
+        Rect leftRect = LeftPanel.rect;
+        Vector2 leftDir = TwinStickControls.getLeftDirection();
+        LStickIcon.anchoredPosition =
+            new Vector2(leftDir.x * leftRect.width / 2.0f, leftDir.y * leftRect.height / 2.0f);
+
+        Rect rightRect = RightPanel.rect;
+        Vector2 rightDir = TwinStickControls.getRightDirection();
+        RStickIcon.anchoredPosition =
+            new Vector2(rightDir.x * rightRect.width / 2.0f, rightDir.y * rightRect.height / 2.0f);
+    }
 }
