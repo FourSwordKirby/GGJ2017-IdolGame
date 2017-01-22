530edf8a72833c4d34516e41019fcdcbd7b9f01d
FourSwordKirby <rogliu26@gmail.com>
2017-01-22 13:02:30 -0500

    added Daniels graphics

---
+++ Assets/Scripts/SpotlightRotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotlightRotate : MonoBehaviour {
+    public AnimationCurve curve;
+    public float angle;
+    void Update()
+    {
+        transform.localEulerAngles = Vector3.forward * (0.5f * angle * curve.Evaluate(Time.time));
+    }
+}
