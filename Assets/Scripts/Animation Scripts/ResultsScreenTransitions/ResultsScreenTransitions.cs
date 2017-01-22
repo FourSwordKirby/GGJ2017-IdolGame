fd631644419d6dc489efa955601cd9043596cb9e
FourSwordKirby <rogliu26@gmail.com>
2017-01-22 05:50:41 -0500

    Almost finished the ending screen, going to kind of hook it into the main concert scene

---
+++ Assets/Scripts/Animation Scripts/ResultsScreenTransitions/ResultsScreenTransitions.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultsScreenTransitions : MonoBehaviour {
+
+    //Temporarily Hardcoded
+    public static float score = 120;
+    public static List<float> accumulatedScores = new List<float>() { 5.0f, 15.0f, 30.0f, 50.0f, 100.0f, 120.0f};
+
+    public ScoreController scoreController;
+    public ScoreChart scoreChart;
+    public NameController nameController;
+
+    public ScreenFader fader;
+    public bool titleScreenSelected;
+
+    // Use this for initialization
+    void Start () {
+        scoreController.SetScore(score);
+        scoreChart.DisplayScores(accumulatedScores);
+        StartCoroutine(fader.FadeIn());
+    }
+
+    public void BackToTitle()
+    {
+        if (!fader.fading)
+        {
+            StartCoroutine(fader.FadeOut());
+            titleScreenSelected = true;
+        }
+    }
+
+    public void SubmitHighScore()
+    {
+        string name = nameController.GetName();
+        float score = ResultsScreenTransitions.score;
+
+        string filePath = Application.streamingAssetsPath + "/HighScores.txt";
+        string contents = File.ReadAllText(filePath);
+        contents += name + " " + score.ToString() + "\n";
+        File.WriteAllText(filePath, contents);
+        Debug.Log("wrote to log");
+
+        //Remove the submit button etc.
+    }
+
+    void Update()
+    {
+        if (titleScreenSelected)
+        {
+            if (!fader.fading)
+                SceneManager.LoadScene(0);
+        }
+    }
+}
--- Assets/Scripts/Animation Scripts/TitleScreenAnim/StartScreenTransitions.cs
+++ Assets/Scripts/Animation Scripts/TitleScreenAnim/StartScreenTransitions.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class StartScreenTransitions : MonoBehaviour {
