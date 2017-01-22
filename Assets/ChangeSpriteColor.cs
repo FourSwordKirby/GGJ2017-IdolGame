99f6f678ed3c2c902e1c15e058b20f2290479921
FourSwordKirby <rogliu26@gmail.com>
2017-01-22 14:05:05 -0500

    Stage lights

---
+++ Assets/ChangeSpriteColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSpriteColor : MonoBehaviour {
+
+    public Gradient color;
+    private SpriteRenderer s;
+
+	// Use this for initialization
+	void Start () {
+        s = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        s.color = color.Evaluate(Time.time % 1);
+	}
+}
