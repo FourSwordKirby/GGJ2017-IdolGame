e2db33dbe9b7830b1d6efe34d3fc9dfad7304f10
FourSwordKirby <rogliu26@gmail.com>
2017-01-21 21:05:28 -0500

    made it so we can animate audience members

--- Assets/Scripts/Animation Scripts/AudienceMember.cs
+++ Assets/Scripts/Animation Scripts/AudienceMember.cs
@@ -9,22 +9,96 @@ public class AudienceMember : MonoBehaviour {
     public GameObject rightArm;
 
     public Animator AudienceAnimator;
-    public float ArmRestingHeight = 1.4f;
-    public float ArmPumpingHeight = -0.6f;
-    public float ArmSlowWaveHeight = 0.7f;
-    public float ArmCrowdWaveScale = 0.7f;
+    private float ArmRestingHeight = 4.2f;
+    private float ArmPumpingHeight = 4.2f;
+    private float ArmSlowWaveHeight = 3.5f;
 
+    private float ArmCrowdWaveScale = 0.75f;
 
+    private bool InTransition;
 
     // Update is called once per frame
     void Update () {
-        if(currentGesture == Gesture.Idle)
+        if(Input.GetKeyDown(KeyCode.Q))
         {
-            //leftArm.transform.position = Vector3.MoveTowards(leftArm.transform.position, );
+            PerformGesture(Gesture.Idle);
         }
-        else if(currentGesture == Gesture.LeftArmPumps)
+        if (Input.GetKeyDown(KeyCode.W))
         {
+            PerformGesture(Gesture.RightArmPumps);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PerformGesture(Gesture.SlowWave);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            PerformGesture(Gesture.CrowdWave);
+        }
 
+        if (currentGesture == Gesture.CrowdWave)
+        {
+            if (AudienceAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+                PerformGesture(Gesture.Idle);
         }
-	}
+
+        //Transitioning between animation states. Doesn't look that good so not really worth it
+        //if(InTransition)
+        //{
+        //    if (currentGesture == Gesture.Idle)
+        //    {
+        //        if (leftArm.transform.localPosition.y == ArmRestingHeight && rightArm.transform.localPosition.y == ArmRestingHeight
+        //            && leftArm.transform.up == Vector3.up && rightArm.transform.up == Vector3.up)
+        //        {
+        //            InTransition = false;
+        //            AudienceAnimator.Play("AudienceIdle");
+        //        }
+        //        else
+        //        {
+        //            leftArm.transform.localPosition = Vector3.MoveTowards(leftArm.transform.localPosition, new Vector3(-0.9f, ArmRestingHeight, 0.05f), 0.03f);
+        //            rightArm.transform.localPosition = Vector3.MoveTowards(rightArm.transform.localPosition, new Vector3(0.9f, ArmRestingHeight, 0.05f), 0.03f);
+        //            leftArm.transform.rotation = Quaternion.RotateTowards(leftArm.transform.rotation, Quaternion.Euler(0, 0, 0), 3f);
+        //            rightArm.transform.rotation = Quaternion.RotateTowards(rightArm.transform.rotation, Quaternion.Euler(0, 0, 0), 3f);
+        //        }
+        //    }
+        //    else if (currentGesture == Gesture.RightArmPumps)
+        //    {
+        //        if (leftArm.transform.localPosition.y == ArmRestingHeight && rightArm.transform.localPosition.y == ArmPumpingHeight
+        //            && leftArm.transform.rotation == Quaternion.Euler(0, 0, 0) && rightArm.transform.rotation == Quaternion.Euler(179, 0, 0))
+        //            PlayAnimation("AudienceRightArmPump");
+        //        else
+        //        {
+        //            leftArm.transform.localPosition = Vector3.MoveTowards(leftArm.transform.localPosition, new Vector3(-0.9f, ArmRestingHeight, 0.05f), 0.03f );
+        //            rightArm.transform.localPosition = Vector3.MoveTowards(rightArm.transform.localPosition, new Vector3(0.9f, ArmPumpingHeight, 0.05f), 0.03f);
+        //            leftArm.transform.rotation = Quaternion.RotateTowards(leftArm.transform.rotation, Quaternion.Euler(0, 0, 0), 3f);
+        //            rightArm.transform.rotation = Quaternion.RotateTowards(rightArm.transform.rotation, Quaternion.Euler(179, 0, 0), 3f);
+        //        }
+        //    }
+        //}
+    }
+
+    void PerformGesture(Gesture g)
+    {
+        AudienceAnimator.enabled = false;
+        currentGesture = g;
+        if (currentGesture == Gesture.Idle)
+            PlayAnimation("AudienceIdle");
+        if (currentGesture == Gesture.LeftArmPumps)
+            PlayAnimation("AudienceLeftArmPump");
+        if (currentGesture == Gesture.RightArmPumps)
+            PlayAnimation("AudienceRightArmPump");
+        if (currentGesture == Gesture.SimultaneousArmPumps)
+            PlayAnimation("AudienceBothArmPump");
+        if (currentGesture == Gesture.SlowWave)
+            PlayAnimation("AudienceBothArmWave");
+        if (currentGesture == Gesture.CrowdWave)
+            PlayAnimation("AudienceCrowdWave");
+    }
+
+    void PlayAnimation(string animName)
+    {
+        InTransition = false;
+        AudienceAnimator.enabled = true;
+        AudienceAnimator.Play(animName);
+    }
 }
